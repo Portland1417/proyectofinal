0c94e115b8999bf9d4fd9e27b062fe48ee8c5cb8
Portland1417 <alejandroorlandini84@gmail.com>
2022-12-09 20:12:19 -0300

    Update app.module.ts

--- ProyectoIntegrador/src/app/app.module.ts
+++ ProyectoIntegrador/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
-import { BannerComponent } from './componets/banner/banner.component';
+import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
